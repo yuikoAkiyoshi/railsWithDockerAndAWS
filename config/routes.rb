0308a217a6551ba2507e72110a64fd61838730b7
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-07-31 15:11:10 +0900

    articlesテーブル周り実装 テーブル作成・CRUD機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "articles/index" => "articles#index"
+  get "articles/new" => "articles#new"
+  post "articles/create" => "articles#create"
+  get "articles/:id" => "articles#show"
+  get "articles/:id/edit" => "articles#edit"
+  post "articles/:id/update" => "articles#update"
+  post "articles/:id/destroy" => "articles#destroy"
 end
