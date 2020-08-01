810aca8052de2c1e5311c7805b47cc7c0c233905
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-08-01 14:50:55 +0900

    tagsテーブル周り実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     has_many :comments, dependent: :delete_all
+    has_many :article_tag_relations, dependent: :delete_all
+    has_many :tags, through: :article_tag_relations
 
     validates :title, presence: true, length:{ maximum: 40 }
     validates :body, presence: true, length:{ maximum: 2000 }
