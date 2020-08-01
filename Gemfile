999b770050828c4ee1f217df1d82ea61f4d3f066
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-08-01 23:35:42 +0900

    画像投稿機能実装

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'kaminari'
 gem 'rails-i18n'
+gem 'carrierwave'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,6 @@ class Article < ApplicationRecord
 
     validates :title, presence: true, length:{ maximum: 40 }
     validates :body, presence: true, length:{ maximum: 2000 }
+
+    mount_uploader :image, ImageUploader
 end
