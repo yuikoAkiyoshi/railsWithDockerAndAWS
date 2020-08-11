1f036db1919493b9f3b642d73e8533a4e8c19a6a
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-08-12 08:42:40 +0900

    usersテーブル周り実装 画像アップロード機能追加、「ユーザーで絞り込む」実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get 'loginform', to: 'users#login'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get 'users/:id/articles', to: 'users#articles'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
-  resources :users, only:%i[create new edit update]
+  resources :users, only:%i[create new edit update destroy index]
   resources :articles
   resources :comments, only:%i[create destroy]
 end
