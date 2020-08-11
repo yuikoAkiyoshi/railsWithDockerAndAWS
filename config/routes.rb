c331e779b0be3dad1ab935815650cd9c50f59df8
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-08-11 21:18:25 +0900

    userとarticlesの紐付け 実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
-  resources :users, only:%i[create new]
+  resources :users, only:%i[create new edit update]
   resources :articles
   resources :comments, only:%i[create destroy]
 end
