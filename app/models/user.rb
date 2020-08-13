755edb8055f6501c51bc0af8daad157bc08b05e6
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-08-13 09:53:13 +0900

    フォロー機能実装 ユーザー一覧画面でのフォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
     has_many :articles, dependent: :delete_all
     has_many :comments, dependent: :delete_all
+    has_many :relationships
+    # relationshipsテーブルのfollow_idを参考にして、followingsモデルにアクセス。user.followingsでフォローしているUserを取得できる
+    has_many :followings, through: :relationships, source: :follow
+    # relaitonshipsテーブルにアクセスする時、follow_idを入口とする
+    has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+    has_many :followers, through: :reverse_of_relationships, source: :user
 
     # password属性とpassword_confirmation属性がUserモデルに追加（二回パスワード記入して正しいパスワードかチェック）
     has_secure_password
@@ -17,4 +23,19 @@ class User < ApplicationRecord
         length: { minimum: 8 }
     
     mount_uploader :image, ImageUploader
+
+    def follow(other_user)
+        unless self == other_user
+          self.relationships.find_or_create_by(follow_id: other_user.id)
+        end
+    end
+
+    def unfollow(other_user)
+        relationship = self.relationships.find_by(follow_id: other_user.id)
+        relationship.destroy if relationship
+    end
+
+    def following?(other_user)
+        self.followings.include?(other_user)
+    end
 end
